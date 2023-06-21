eb44fb3b7c93333c430ce21a584c320b4652520f
Alex <alex@konstankino.com>
2023-06-21 11:12:16 +0300

    Mapped doctors after getting

--- src/modules/doctor/pages/Doctor/index.tsx
+++ src/modules/doctor/pages/Doctor/index.tsx
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react";
 import DoctorForm from "../../components/DoctorForm";
 import DoctorApI from "../../api";
-import { City, DoctorInfo, Speciality } from "../../types";
+import { City, DoctorDefaultInfo, DoctorFullInfo, Speciality } from "../../types";
 import { Spin } from "antd";
 
 const Doctor = () => {
   const [cities, setCities] = useState<City[] | null>(null);
   const [specialities, setSpecialities] = useState<Speciality[] | null>(null);
-  const [doctors, setDoctors] = useState<DoctorInfo[] | null>(null);
+  const [doctors, setDoctors] = useState<DoctorFullInfo[] | null>(null);
 
   useEffect(() => {
     (async () => {
@@ -16,7 +16,21 @@ const Doctor = () => {
       );
       setCities(cities);
       setSpecialities(specialities);
-      setDoctors(doctors);
+      const mapDoctors = doctors.map((doctor: DoctorDefaultInfo) => {
+        const speciality = specialities.find((speciality: Speciality) => speciality.id === doctor.specialityId);
+        const city = cities.find((city: City) => city.id === doctor.cityId);
+        return {
+          id: doctor.id,
+          name: `${doctor.name} ${doctor.surname}`,
+          specialityName: speciality?.name,
+          maxAge: speciality?.params?.maxAge || null,
+          minAge: speciality?.params?.minAge || null,
+          gender: speciality?.params?.gender || null,
+          cityName: city?.name,
+          isPediatrician: doctor.isPediatrician
+        }
+      })
+      setDoctors(mapDoctors);
     })();
   }, []);
 
